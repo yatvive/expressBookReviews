b665de7a0ab44f4af4e14fe7f7cf530ca0caafe6
yatvive <215081267+yatvive@users.noreply.github.com>
2026-09-12 21:05:38 -0400

    Feat: Implement Task 10 using async-await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,120 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Vérifier si le nom d'utilisateur et le mot de passe sont fournis
+  if (username && password) {
+    // Vérifier si l'utilisateur existe déjà dans le tableau 'users'
+    const userExists = users.some((user) => user.username === username);
+
+    if (!userExists) {
+      // Ajouter le nouvel utilisateur
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "Utilisateur enregistré avec succès. Vous pouvez vous connecter." });
+    } else {
+      return res.status(400).json({ message: "Cet utilisateur existe déjà !" });
+    }
+  }
+
+  return res.status(400).json({ message: "Nom d'utilisateur ou mot de passe manquant." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Tâche 10 : Obtenir la liste des livres disponibles en utilisant des callbacks de Promesse ou async-await
+public_users.get('/', async function (req, res) {
+    // Création d'une promesse pour simuler une opération asynchrone
+    const getBooksPromise = new Promise((resolve, reject) => {
+      if (books) {
+        resolve(books);
+      } else {
+        reject("Impossible de charger les livres.");
+      }
+    });
+  
+    try {
+      // Attente de la résolution de la promesse (Asynchrone via async/await)
+      const availableBooks = await getBooksPromise;
+      return res.status(200).send(JSON.stringify(availableBooks, null, 4));
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  });
+  
 
-// Get book details based on ISBN
+  // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Récupère l'ISBN depuis les paramètres de l'URL
+  const isbn = req.params.isbn;
+  
+  // Extrait le livre correspondant de l'objet 'books'
+  const book = books[isbn];
+
+  if (book) {
+    // Renvoie le livre trouvé avec un statut 200 OK
+    return res.status(200).send(JSON.stringify(book, null, 4));
+  } else {
+    // Renvoie une erreur si l'ISBN n'existe pas
+    return res.status(404).json({ message: "Livre non trouvé" });
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorParam = req.params.author.toLowerCase();
+  const keys = Object.keys(books); // Indice 1 : Obtenir toutes les clés
+  const matchingBooks = [];
+
+  // Indice 2 : Itérer à travers le tableau 'books'
+  keys.forEach(key => {
+    if (books[key].author.toLowerCase() === authorParam) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  });
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).json({ message: "Aucun livre trouvé pour cet auteur" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const titleParam = req.params.title.toLowerCase();
+  const keys = Object.keys(books);
+  const matchingBooks = [];
+
+  // Parcourir tous les livres pour trouver les titres correspondants
+  keys.forEach(key => {
+    if (books[key].title.toLowerCase() === titleParam) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  });
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 4));
+  } else {
+    return res.status(404).json({ message: "Aucun livre trouvé avec ce titre" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book) {
+    // Renvoie uniquement l'objet contenant les avis du livre
+    return res.status(200).send(JSON.stringify(book.reviews, null, 4));
+  } else {
+    return res.status(404).json({ message: "Livre non trouvé" });
+  }
 });
 
 module.exports.general = public_users;
