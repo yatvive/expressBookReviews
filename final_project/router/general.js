20c2968f39c8dbeaf9e318cffd9909636ca56371
yatvive <215081267+yatvive@users.noreply.github.com>
2026-09-12 21:31:34 -0400

    Feat: Implement Task 11 using async-await for ISBN search

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -49,23 +49,47 @@ public_users.get('/', async function (req, res) {
   });
   
 
-  // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  // Récupère l'ISBN depuis les paramètres de l'URL
-  const isbn = req.params.isbn;
+//   // Get book details based on ISBN
+// public_users.get('/isbn/:isbn',function (req, res) {
+//   //Write your code here
+//   // Récupère l'ISBN depuis les paramètres de l'URL
+//   const isbn = req.params.isbn;
   
-  // Extrait le livre correspondant de l'objet 'books'
-  const book = books[isbn];
+//   // Extrait le livre correspondant de l'objet 'books'
+//   const book = books[isbn];
 
-  if (book) {
-    // Renvoie le livre trouvé avec un statut 200 OK
-    return res.status(200).send(JSON.stringify(book, null, 4));
-  } else {
-    // Renvoie une erreur si l'ISBN n'existe pas
-    return res.status(404).json({ message: "Livre non trouvé" });
-  }
- });
+//   if (book) {
+//     // Renvoie le livre trouvé avec un statut 200 OK
+//     return res.status(200).send(JSON.stringify(book, null, 4));
+//   } else {
+//     // Renvoie une erreur si l'ISBN n'existe pas
+//     return res.status(404).json({ message: "Livre non trouvé" });
+//   }
+//  });
+
+// Tâche 11 : Obtenir les détails du livre en fonction de l'ISBN en utilisant des Promesses ou async-await
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+  
+    // Création d'une promesse pour simuler une opération d'extraction asynchrone
+    const getBookByIsbnPromise = new Promise((resolve, reject) => {
+      const book = books[isbn];
+      if (book) {
+        resolve(book);
+      } else {
+        reject("Aucun livre trouvé avec cet ISBN.");
+      }
+    });
+  
+    try {
+      // Attente de la résolution de la promesse
+      const bookDetails = await getBookByIsbnPromise;
+      return res.status(200).send(JSON.stringify(bookDetails, null, 4));
+    } catch (error) {
+      return res.status(404).json({ message: error });
+    }
+  });
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
